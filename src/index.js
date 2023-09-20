1004943d43b08d63058322e877d547f1b23d06c4
Anthony Lima <anthonylimac@gmail.com>
2023-09-20 04:04:12 -0300

    second commmit, just testing

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// 1- import the React and ReactDOM libraries
+
+// 2- Get a reference to the div with ID root
+
+// 3- tell react to take control of that element
+
+// 4- Create a component
+
+// 5- Show the component on the screen
+
+// 6- just testing
